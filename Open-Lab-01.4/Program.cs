dc6d44ccf3810ccef75fcb853bf85adc9cc2b669
samuel zaťovič <szatovic@gmail.com>
2019-10-13 15:53:42 +0200

    Hotový program

---
+++ Open-Lab-01.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_01._4
+{
+    class Program
+    {
+       public static double TriArea(double side, double height)
+        {
+            double result = side * height * 0.5;
+            return result;
+        }
+    }
+}
